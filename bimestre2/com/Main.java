d9b7b0320afd29f8c34e9ca4ba2ede438485842f
YuSuzukii <126773140+YuSuzukii@users.noreply.github.com>
2023-05-25 08:52:45 -0300

    Delete bimestre2/com directory

--- bimestre2/com/Main.java
+++
@@ -1,8 +0,0 @@
-package com.heranca;
-import com.heranca.*;
-public class Main {
-
-    funcionario f1= new funcionario("Miyavi", "wangwang", 30, 34000);
-    cliente c1= new cliente("BI","ikonic", 27, "pago");
-
-}
--- bimestre2/com/heranca/Fixo.java
+++
@@ -1,5 +0,0 @@
-package com.tarefa;
-
-public class Fixo {
-
-}
